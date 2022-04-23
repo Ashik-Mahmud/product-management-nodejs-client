eb12e9fd5689cbd8538fd4619942bcd9f0f77abc
ashikmahmud <ashikmamud187@gmail.com>
2022-04-23 19:38:44 +0600

    now we can update of my product

--- src/Pages/ManageProduct/ManageProduct.jsx
+++ src/Pages/ManageProduct/ManageProduct.jsx
@@ -1,51 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../Firebase/Firebase.config";
+import useProducts from "../../Hooks/useProducts";
 import "../Pages.css";
 const ManageProduct = () => {
+  const navigate = useNavigate();
+  const { products } = useProducts();
+  const [currentUserProducts, setCurrentUserProducts] = useState([]);
+  useEffect(() => {
+    const userProducts = products.filter(
+      (product) => product?.author?.uid === auth?.currentUser?.uid
+    );
+    setCurrentUserProducts(userProducts);
+  }, [products]);
+
   return (
     <div>
       <div className="container">
         <div className="table-wrapper">
           <h1>Manage Product</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Category</th>
-                <th>Brand</th>
-                <th>Image</th>
-                <th width="100px">Edit</th>
-                <th width="100px">Delete</th>
-                <th width="100px">Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Laptop</td>
-                <td>3243</td>
-                <td>Laptop</td>
-                <td>ASUS</td>
-                <td>
-                  <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTe_5kPXQV4S6jTkKb0zcV9NVYzo2TjLLt3yDBcRZ2Yd3VFPqfFwtcS8Hex5ikZnm0QcNI&usqp=CAU"
-                    alt="product"
-                    width={100}
-                  />
-                </td>
-                <td>
-                  <span className="btn btn-primary">Edit</span>
-                </td>
-                <td>
-                  <span className="btn btn-danger">Delete</span>
-                </td>
-                <td>
-                  <span className="colorize">Published</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          {currentUserProducts.length > 0 ? (
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Price</th>
+                  <th>Category</th>
+                  <th>Brand</th>
+                  <th>Image</th>
+                  <th width="100px">Edit</th>
+                  <th width="100px">Delete</th>
+                  <th width="100px">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {currentUserProducts.map((product, ind) => (
+                  <tr key={product._id}>
+                    <td>{ind + 1}</td>
+                    <td>{product?.productName.slice(0, 40)}</td>
+                    <td>{product?.price}</td>
+                    <td>{product?.category}</td>
+                    <td>{product?.brand}</td>
+                    <td>
+                      <img src={product?.photoUrl} alt="product" width={100} />
+                    </td>
+                    <td>
+                      <span
+                        onClick={() =>
+                          navigate(`/update-product/${product?._id}`)
+                        }
+                        className="btn btn-primary"
+                      >
+                        Edit
+                      </span>
+                    </td>
+                    <td>
+                      <span className="btn btn-danger">Delete</span>
+                    </td>
+                    <td>
+                      <span className="colorize">Published</span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div style={{ textAlign: "center", padding: "4rem 0rem" }}>
+              <h3>No Product added yet.</h3>
+              <button
+                onClick={() => navigate("/create-product")}
+                style={{ margin: "1rem auto" }}
+                className="btn btn-primary"
+              >
+                Add Product
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/Pages/UpdateProduct/UpdateProduct.jsx
+++ src/Pages/UpdateProduct/UpdateProduct.jsx
@@ -1,10 +1,124 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useNavigate, useParams } from "react-router-dom";
+import { auth } from "../../Firebase/Firebase.config";
+import useProducts from "../../Hooks/useProducts";
 
 const UpdateProduct = () => {
+  const { id } = useParams();
+  const { products } = useProducts();
+  const currentProduct = products.find((product) => product._id === id);
+  const formRef = useRef(null);
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
+  const onSubmit = async (data) => {
+    const newData = {
+      ...data,
+      author: {
+        name: auth?.currentUser?.displayName,
+        uid: auth?.currentUser?.uid,
+      },
+    };
+
+    await axios
+      .put(`http://localhost:5000/products/${id}`, newData)
+      .then(function (response) {
+        if (response?.data?.acknowledged) {
+          toast.success(`Product Update successfully done.`);
+          navigate("/manage-product");
+        } else {
+          toast.error(`Something went wrong`);
+        }
+      });
+    formRef.current.reset();
+  };
+
   return (
     <div>
       <div className="container">
-        <h1>UpdateProduct</h1>
+        <form
+          ref={formRef}
+          className="form-wrapper"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <h1>Update Product </h1>
+          <div className="input-group">
+            <label htmlFor="buying-url">Product Buying URL</label>
+            <input
+              type="url"
+              placeholder="Product Buying URL"
+              id="buying-url"
+              {...register("buyingUrl")}
+              defaultValue={currentProduct?.buyingUrl}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="name">Product Name</label>
+            <input
+              type="text"
+              placeholder="Product Name"
+              id="name"
+              {...register("productName", { required: true })}
+              defaultValue={currentProduct?.productName}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="category">Category</label>
+            <input
+              type="text"
+              placeholder="Product Category"
+              id="category"
+              {...register("category", { required: true })}
+              defaultValue={currentProduct?.category}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="price">Product Price</label>
+            <input
+              type="text"
+              placeholder="Product Price"
+              id="price"
+              {...register("price", { required: true })}
+              defaultValue={currentProduct?.price}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="brand">Product Brand</label>
+            <input
+              type="text"
+              placeholder="Product Brand"
+              id="brand"
+              {...register("brand", { required: true })}
+              defaultValue={currentProduct?.brand}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="name">Short Description</label>
+            <textarea
+              type="text"
+              placeholder="Product Description"
+              rows={3}
+              id="name"
+              {...register("shortDescription", { required: true })}
+              defaultValue={currentProduct?.shortDescription}
+            ></textarea>
+          </div>
+          <div className="input-group">
+            <label htmlFor="url">Product Image URL</label>
+            <input
+              type="url"
+              {...register("photoUrl", { required: true })}
+              placeholder="Product Image URL"
+              id="url"
+              defaultValue={currentProduct?.photoUrl}
+            />
+          </div>
+          <div className="input-group">
+            <button className="btn btn-primary">Update Product</button>
+          </div>
+        </form>
       </div>
     </div>
   );
