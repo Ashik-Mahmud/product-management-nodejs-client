0a39db524850a66574037b35b08f2b32df3b7366
ashikmahmud <ashikmamud187@gmail.com>
2022-04-25 21:40:24 +0600

    put not order messages

--- src/Pages/ManageOrder/ManageOrder.jsx
+++ src/Pages/ManageOrder/ManageOrder.jsx
@@ -36,47 +36,62 @@ const ManageOrder = () => {
       <div className="container">
         <div className="table-wrapper">
           <h1>Manage Order</h1>
-          <table>
-            <thead>
-              <tr>
-                <th>OrderId</th>
-                <th>Shipping Address</th>
-                <th>Phone</th>
-                <th>Email</th>
-                <th>Product</th>
-                <th>Action</th>
-                <th>Danger</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order) => (
-                <tr key={order._id}>
-                  <td>{order?._id.slice(0, 5)}</td>
-                  <td>{order?.address}</td>
-                  <td>{order?.phone}</td>
-                  <td>{order?.author?.email}</td>
-                  <td>
-                    <span
-                      onClick={() =>
-                        navigate(`/view-order/${order?.productId}`)
-                      }
-                      className="colorize cursor-pointer"
-                    >
-                      View
-                    </span>
-                  </td>
-                  <td>
-                    <span className="colorize">Pending</span>
-                  </td>
-                  <td>
-                    <span className="text-danger cursor-pointer">
-                      Cancel Request
-                    </span>
-                  </td>
+          {orders.length > 0 ? (
+            <table>
+              <thead>
+                <tr>
+                  <th>OrderId</th>
+                  <th>Shipping Address</th>
+                  <th>Phone</th>
+                  <th>Email</th>
+                  <th>Product</th>
+                  <th>Action</th>
+                  <th>Danger</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {orders.map((order) => (
+                  <tr key={order._id}>
+                    <td>{order?._id.slice(0, 5)}</td>
+                    <td>{order?.address}</td>
+                    <td>{order?.phone}</td>
+                    <td>{order?.author?.email}</td>
+                    <td>
+                      <span
+                        onClick={() =>
+                          navigate(`/view-order/${order?.productId}`)
+                        }
+                        className="colorize cursor-pointer"
+                      >
+                        View
+                      </span>
+                    </td>
+                    <td>
+                      <span className="colorize">Pending</span>
+                    </td>
+                    <td>
+                      <span className="text-danger cursor-pointer">
+                        Cancel Request
+                      </span>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <>
+              <div style={{ textAlign: "center", padding: "4rem 0rem" }}>
+                <h3>No Order yet.</h3>
+                <button
+                  onClick={() => navigate("/")}
+                  style={{ margin: "1rem auto" }}
+                  className="btn btn-primary"
+                >
+                  Order Product
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </section>
